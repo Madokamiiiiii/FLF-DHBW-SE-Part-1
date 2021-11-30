e1ea90ad0d9104eb8715d61714686dd04ad457a1
Gabriel Nill <gabriel.nill@outlook.de>
2021-11-30 16:03:35 +0100

    nobs

--- src/main/java/airportfiretruck/cabin/panel/rotaryknobs/FrontThrowerKnob.java
+++ src/main/java/airportfiretruck/cabin/panel/rotaryknobs/FrontThrowerKnob.java
@@ -3,16 +3,34 @@ package airportfiretruck.cabin.panel.rotaryknobs;
 public class FrontThrowerKnob implements IRotaryKnob{
     private int level;
     public FrontThrowerKnob() {
-
+        level = 0;
     }
 
     @Override
     public void turnLeft() {
-
+        switch(level) {
+            case 500 -> level=0;
+            case 1000 -> level=500;
+            case 1500 -> level=1000;
+            case 2000 -> level=1500;
+            case 2500 -> level=2000;
+            case 3000 -> level=2500;
+            case 3500 -> level=3000;
+            default -> {}
+        }
     }
 
     @Override
     public void turnRight() {
-
+        switch(level) {
+            case 0 -> level=500;
+            case 500 -> level=1000;
+            case 1000 -> level=1500;
+            case 1500 -> level=2000;
+            case 2000 -> level=2500;
+            case 2500 -> level=3000;
+            case 3000 -> level=3500;
+            default -> {}
+        }
     }
 }
--- src/main/java/airportfiretruck/cabin/panel/rotaryknobs/RoofThrowerKnob.java
+++ src/main/java/airportfiretruck/cabin/panel/rotaryknobs/RoofThrowerKnob.java
@@ -6,16 +6,24 @@ public class RoofThrowerKnob implements IRotaryKnob{
     private RoofThrowerLevel level;
 
     public RoofThrowerKnob() {
-
+        level = RoofThrowerLevel.A;
     }
 
     @Override
     public void turnLeft() {
-
+        switch (level) {
+            case A -> { }
+            case B -> level = RoofThrowerLevel.A;
+            case C -> level = RoofThrowerLevel.B;
+        }
     }
 
     @Override
     public void turnRight() {
-
+        switch (level) {
+            case A -> level = RoofThrowerLevel.B;
+            case B -> level = RoofThrowerLevel.C;
+            case C -> { }
+        }
     }
 }
