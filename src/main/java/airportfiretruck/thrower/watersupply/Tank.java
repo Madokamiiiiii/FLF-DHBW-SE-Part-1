7dd73fc9d3d2076a18b9ae194e767641d9ce0cbd
Gabriel Nill <gabriel.nill@outlook.de>
2021-12-01 12:00:54 +0100

    added tank brainfuck

--- src/main/java/airportfiretruck/thrower/watersupply/Tank.java
+++ src/main/java/airportfiretruck/thrower/watersupply/Tank.java
@@ -1,16 +1,78 @@
 package airportfiretruck.thrower.watersupply;
 
 public class Tank {
-    private int capacity;
+    private int[][][] capacity;
     private ExtinguishingAgent type;
+    private int[] pointer;
 
-    public Tank() {
-
+    public Tank(ExtinguishingAgent type) {
+        this.type = type;
+        switch (type) {
+            case FOAM -> {
+                capacity = new int[25][10][10];
+                pointer = new int[]{24,9,0};
+            }
+            case WATER -> {
+                capacity = new int[50][25][10];
+                pointer = new int[]{49,24,0};
+            }
+        }
     }
     public void fill(int amount) {
-
+        //oben links : 0,0,0 -> unten rechts : max,max,max
+        for (;amount > 0;amount--) {
+            if ( pointer[1] < capacity[0].length) {
+                if (pointer[0] < capacity.length) {
+                    if (pointer[2] > 0) {
+                        capacity[pointer[0]][pointer[1]][pointer[2]] = 1;
+                        pointer[2]--;
+                        continue;
+                    }
+                    pointer[2]=capacity[0][0].length;
+                    pointer[0]++;
+                    capacity[pointer[0]][pointer[1]][pointer[2]] = 1;
+                    pointer[2]--;
+                    continue;
+                }
+                pointer[2]=capacity[0][0].length;
+                pointer[0]= 0;
+                pointer[1]++;
+                capacity[pointer[0]][pointer[1]][pointer[2]] = 1;
+                pointer[2]--;
+                continue;
+            }
+            return;
+        }
     }
     public int getLiquid(int amount) {
-        return 0;
+        //oben links : 0,0,0 -> unten rechts : max,max,max
+        int pumped = 0;
+        for (;amount > 0;amount--) {
+            if ( pointer[1] > 0) {
+                if (pointer[0] > 0) {
+                    if (pointer[2] < capacity[0][0].length-1) {
+                        capacity[pointer[0]][pointer[1]][pointer[2]] = 0;
+                        pointer[2]++;
+                        pumped++;
+                        continue;
+                    }
+                    pointer[2]=0;
+                    pointer[0]--;
+                    capacity[pointer[0]][pointer[1]][pointer[2]] = 0;
+                    pointer[2]++;
+                    pumped++;
+                    continue;
+                }
+                pointer[2]=0;
+                pointer[0]= capacity.length-1;
+                pointer[1]--;
+                capacity[pointer[0]][pointer[1]][pointer[2]] = 0;
+                pointer[2]++;
+                pumped++;
+                continue;
+            }
+            return pumped;
+        }
+        return pumped;
     }
 }
