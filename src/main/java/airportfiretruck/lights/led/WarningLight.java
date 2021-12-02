4a66ecbc256b1ad661b0c24dd0b8695667ec40cd
Gabriel Nill <gabriel.nill@outlook.de>
2021-12-02 09:59:44 +0100

    improved light implementations...no clue bout composition tho

--- src/main/java/airportfiretruck/lights/led/WarningLight.java
+++ src/main/java/airportfiretruck/lights/led/WarningLight.java
@@ -2,7 +2,13 @@ package airportfiretruck.lights.led;
 
 import airportfiretruck.lights.Light;
 
+import java.util.List;
+
 public class WarningLight extends Light {
     final Color color = Color.ORANGE;
+    private Led led;
     // composition mit Led
+    public WarningLight() {
+        this.led=new Led(Color.ORANGE);
+    }
 }
